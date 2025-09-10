7dcb2ac33c95a8614eb14375bafad8f8c33639e5
Kos Russell <kosmondcrussell@gmail.com>
2025-09-10 04:14:32 -0700

    add typescript configs

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { Category } from './types';
+
+export const categories: Category[] = [
+  { name: 'Personal To-Do', type: 'todo', icon: 'CheckSquare', color: 'text-blue-400' },
+  { name: 'Project Idea', type: 'idea', icon: 'Lightbulb', color: 'text-yellow-400' },
+  { name: 'Meeting Note', type: 'meeting', icon: 'FileText', color: 'text-green-400' }
+];
